a03b68c1fed6553d46686d6d2d70292001189c1c
Ilya Ershov <isershov@edu.hse.ru>
2025-01-04 00:00:44 +0300

    add fileinput support

--- main.cpp
+++ main.cpp
@@ -1,23 +1,43 @@
 #include <iostream>
+#include <filesystem>
 #include "species.h"
 #include "genetic.h"
 #include "parser_b.h"
 #include "parser_e.h"
 
-int main() {
-    auto parser = NCVRP::NParser::TParserE();
-    bool result = parser.Parse("E-n13-k4.vrp.txt");
-    if (result) {
-        std::cout << "Parsing successful" << std::endl;
-    } else {
-        std::cout << "Parsing failed" << std::endl;
-        exit(1);
+int main(int argc, char* argv[]) {
+     if (argc < 2) {
+        std::cerr << "Ошибка: укажите путь до директории в аргументах!\n";
+        return 1;
     }
-    NCVRP::NGenetic::TGeneticAlgorithm<TSpecies> geneticAlgorithm(200, 10, 0.5, 1000, parser.GetCapacity());
-    std::cout << "Genetic algorithm started" << std::endl;
-    auto res = geneticAlgorithm.Calculation(parser.GetDistanceMatrix(), parser.GetDemandVector());
 
-    std::cout << "Result: " << res.second << std::endl;
-    geneticAlgorithm.FitnessPrint(res.first.GetGens(), parser.GetDistanceMatrix(), parser.GetDemandVector());
+    std::string directoryPath = argv[1]; // Получаем путь из аргументов
+
+    // Проверяем, существует ли путь и это ли директория
+    if (!std::filesystem::exists(directoryPath)) {
+        std::cerr << "Ошибка: указанный путь не существует!\n";
+        return 1;
+    }
+
+    if (!std::filesystem::is_directory(directoryPath)) {
+        std::cerr << "Ошибка: указанный путь не является директорией!\n";
+        return 1;
+    }
+
+    // Выводим список всех файлов в директории
+    std::cout << "Список файлов в директории: " << directoryPath << "\n";
+    for (const auto& entry : std::filesystem::directory_iterator(directoryPath)) {
+        std::cout << entry.path().filename() << "\n";
+        if (entry.path().string().find("E") == std::string::npos) {
+            continue;
+        }
+        auto parser = NCVRP::NParser::TParserE();
+        bool result = parser.Parse(entry.path().string());
+        NCVRP::NGenetic::TGeneticAlgorithm<TSpecies> geneticAlgorithm(200, 10, 0.5, 1000, parser.GetCapacity());
+        std::cout << "Genetic algorithm started" << std::endl;
+        auto res = geneticAlgorithm.Calculation(parser.GetDistanceMatrix(), parser.GetDemandVector());
+        std::cout << "Result: " << res.second << std::endl;
+        geneticAlgorithm.FitnessPrint(res.first.GetGens(), parser.GetDistanceMatrix(), parser.GetDemandVector());
+    }
     return 0;
 }
